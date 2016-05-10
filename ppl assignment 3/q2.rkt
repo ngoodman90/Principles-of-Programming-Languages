85a72253886b3a27a5696f60a7a11bd7c17eac2c
noam <ngoodman90@gmail.com>
2016-05-10 12:08:18 +0300

    update 3

--- ppl assignment 3/q2.rkt
+++ ppl assignment 3/q2.rkt
@@ -6,13 +6,32 @@
 ; |   Answer Q2-a below   |
 ; +-----------------------+
 
+
+;Signature: derive(f dx)
+;Purpose: to construct a procedure that computes the derivative
+;dx approximation of a function:
+;derive(f dx)(x) = (f(x+dx) - f(x) )/ dx
+;Type: [[Number -> Number]*Number -> [Number -> Number]]
+;Pre-conditions: 0 < dx < 1
+;Tests: ((derive cube 0.001) 5) ==> ~75
+(define derive
+   (lambda (f dx)
+      (lambda (x)
+          (/ (- (f (+ x dx)) (f x))
+              dx))))
+
 ; Signature: c-nth-deriv-f(f)
 ; Type: [[Number -> Number] -> _____________]
 ; Purpose: 
 ; Pre-conditions:
 ; Tests:
 (define c-nth-deriv-f
-  (lambda (f) ...))
+  (lambda (f) 
+    (let ([f1 (derive f 0.001)])
+          (lambda (n)
+            (if (= n 0)
+                f
+                ((c-nth-deriv-f f1) (- n 1)))))))
 
 ; +-----------------------+
 ; |   Answer Q2-b below   |
@@ -24,4 +43,9 @@
 ; Pre-conditions:
 ; Tests:
 (define c-nth-deriv-n
-  (lambda (n) ...))
+  (lambda (n) 
+    (if (= n 0)
+        (lambda (f)
+          f)
+        (lambda (f)
+          ((c-nth-deriv-n (- n 1)) (derive f 0.001))))))
