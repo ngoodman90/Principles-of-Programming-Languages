2bb95b21fae9abf9e03eebd04e55ccabbb0a0604
ngoodman90 <ngoodman90@gmail.com>
2016-06-19 18:00:38 +0300

    final commit: assignment submitted

--- ppl assignment 4/ex4_q5/ex4_q5_tests.rkt
+++ ppl assignment 4/ex4_q5/ex4_q5_tests.rkt
@@ -15,36 +15,4 @@
   (derive-eval 
    '(defined-in-closure? y (foo 5)))
   => '#f)
-  (test
-  (derive-eval 
-   '(define foo2 (lambda (x y) (lambda (z) (lambda (a) x)))))
-  => 'ok)
- (test
-  (derive-eval 
-   '(defined-in-closure? x ((foo2 1 2) 3)))
-  => '#t)
-  (test
-  (derive-eval 
-   '(defined-in-closure? y ((foo2 1 2) 3)))
-  => '#t)
-  (test
-  (derive-eval 
-   '(defined-in-closure? z ((foo2 1 2) 3)))
-  => '#t)
- (test
-  (derive-eval 
-   '(defined-in-closure? a ((foo2 1 2) 3)))
-  => '#f)
-  (test
-  (derive-eval 
-   '(defined-in-closure? b ((foo2 1 2) 3)))
-  => '#f)
-  (test
-  (derive-eval 
-   '(define foo3 (lambda() (lambda(x) 1))))
-  => 'ok)  
-  (test
-    (derive-eval 
-   '(defined-in-closure? x (foo3)))
-  => '#f)
  )
--- ppl assignment 4/ex4_q6/ex4_q6_tests.rkt
+++ ppl assignment 4/ex4_q6/ex4_q6_tests.rkt
@@ -7,13 +7,4 @@
  (test (derive-analyze-eval '(and)) => '#t)
  (test (derive-analyze-eval '(and 1 #f 3)) => '#f)
  (test (derive-analyze-eval '(and 1 #f (/ 1 0))) => '#f)
- (test (derive-analyze-eval '(and 1 ((lambda (x)(= x 5)) 4))) => '#f)
- (test (derive-analyze-eval '(and 1 ((lambda (x)(= x 5)) 5) 5)) => '5)
- (test (derive-analyze-eval '(and 1 ((lambda (x)(= x 5)) 5) (((lambda()(lambda()#f)))) 5)) => '#f)
- (test (derive-analyze-eval '(and 1 ((lambda (x)(= x 5)) 5) ((lambda()(lambda()#f)))4)) => 4)
- (test (derive-analyze-eval '(and #f)) => '#f)
- (test (derive-analyze-eval '(and (= 1 1))) => #t)
- (test (derive-analyze-eval '(and (= 1 2))) => #f)
- (test (derive-analyze-eval '(and 4 4 4 5 5 5 #t (= 1 2))) => #f)
- 
  )
